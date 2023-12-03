3b988cf6d15485259047415fbe801fb7af780a95
Daniel Stroh <strohdaniel624@gmail.com>
2023-12-03 14:09:21 -0700

    Day 3 part 2

--- day3/main.hs
+++ day3/main.hs
@@ -2,13 +2,14 @@ import Data.Char (isDigit)
 import Data.List (groupBy, find)
 import Data.Maybe (isJust)
 
-data Part = PartNumber {
+data PartNumber  = PartNumber {
   number :: Int,
-  line :: Int,
+  num_line :: Int,
   range :: (Int, Int)
-} | Symbol {
+} deriving (Show)
+data Symbol = Symbol {
   symbol :: Char,
-  line :: Int,
+  sym_line :: Int,
   index :: Int
 } deriving (Show)
 
@@ -17,23 +18,26 @@ main = do
   input <- getContents
   let symbols = concatMap getSymbols $ zip [0..] $ lines input
   let numbers = concatMap getNumbers $ zip [0..] $ lines input
-  let missing_parts = filter (\(PartNumber _ nl (b, e)) -> isJust $ find (\(Symbol _ sl i) -> abs (nl - sl) <= 1 && i >= b - 1 && i <= e + 1) symbols) numbers
+  let missing_parts = filter (`isMissing` symbols) numbers
   let s = sum $ map number missing_parts
+
+  let gears = getGears symbols numbers
+  let gear_sum = sum $ map (product. map number) gears
   -- putStr "Symbols: "; print symbols
   -- putStr "Numbers: "; print numbers
   -- putStr "Missing parts: "; print missing_parts
+  -- putStr "Gears: "; print gears
+
   putStr "Sum: "; print s
+  putStr "Gear sum: "; print gear_sum
 
-toParts :: (Int, String) -> [Part]
-toParts (line, str) = []
-
-getSymbols :: (Int, String) -> [Part]
+getSymbols :: (Int, String) -> [Symbol]
 getSymbols (line, str) = map (\(index, symbol) -> Symbol symbol line index ) $ filter (isSymbol . snd) $ zip [0..] str
 
 isSymbol :: Char -> Bool
 isSymbol n = not (isDigit n) && n /= '.'
 
-getNumbers :: (Int, String) -> [Part]
+getNumbers :: (Int, String) -> [PartNumber]
 getNumbers (line, str) =
   let groups = filter (all (isDigit . snd)) $ groupBy (\(_, a) (_, b) -> isDigit a && isDigit b) $ zip [0..] str
       hiranges = map (maximum . map fst) groups
@@ -43,5 +47,12 @@ getNumbers (line, str) =
   in
       zipWith (\r n -> PartNumber n line r) ranges numbers
 
-isPart :: [Part] -> Bool
-isPart n = False
+isAdjacent :: PartNumber -> Symbol -> Bool
+isAdjacent (PartNumber _ nl (b, e)) (Symbol _ sl i) = abs (nl - sl) <= 1 && i >= b - 1 && i <= e + 1
+
+isMissing :: PartNumber -> [Symbol] -> Bool
+isMissing part symbols = isJust $ find (isAdjacent part)  symbols
+
+getGears :: [Symbol] -> [PartNumber] -> [[PartNumber]]
+getGears syms parts = filter (\l -> length l == 2) $ map (`getAdjs` parts) $ filter ((=='*') . symbol) syms
+        where getAdjs sym = filter (`isAdjacent` sym)
