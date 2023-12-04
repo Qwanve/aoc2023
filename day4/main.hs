b90923350ca9adcebad082b13034e6835b1ec9ab
Daniel Stroh <strohdaniel624@gmail.com>
2023-12-04 11:09:12 -0700

    Day 4 part 2

--- day4/main.hs
+++ day4/main.hs
@@ -1,6 +1,8 @@
 import Text.ParserCombinators.Parsec
 import Data.List (intersect)
 
+type Card = ([Int], [Int])
+
 main = do
   input <- getContents
   let cards = case parse parseCards "stdin" input of
@@ -8,9 +10,11 @@ main = do
                   Right result -> result
   let scores = map scoreCard cards
   putStr "Sum: "; print $ sum $ map scoreCard cards
+  let copies = scoreCopyCards $ zip cards (repeat 1)
+  putStr "Copies: "; print copies
 
 
-parseCard :: Parser ([Int], [Int])
+parseCard :: Parser Card
 parseCard = do
   string "Card"
   spaces
@@ -26,15 +30,22 @@ parseCard = do
 parseNumber :: Parser Int
 parseNumber = read <$> many1 digit
   
-parseCards :: Parser [([Int], [Int])]
+parseCards :: Parser [Card]
 parseCards = do
   cards <- many1 parseCard
   eof :: Parser ()
   return cards
   
-scoreCard :: ([Int], [Int]) -> Int
+scoreCard :: Card -> Int
 scoreCard (winners, given) = 
   let len = length (winners `intersect` given) in
     if len > 0 then 2 ^ (len - 1)
     else 0
 
+
+scoreCopyCards :: [(Card, Int)] -> Int
+scoreCopyCards (((winners, given), copies):t) = 
+  let len = length (winners `intersect` given)
+      (head, tail) = splitAt len t in
+      copies + scoreCopyCards ( map (\(card, ncopies) -> (card, ncopies + copies)) head ++ tail)
+scoreCopyCards [] = 0
